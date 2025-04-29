e9c982683cbd72121f5e380c261a8ed7545db380
Biswajit-Credencesoft <biswajit.sahoo@credencesoft.in>
2025-04-29 16:31:44 +0530

    final changes done

--- src/configs/emailjs.js
+++ src/configs/emailjs.js
@@ -1,3 +1,7 @@
-export const serviceId = "service_obe6chx";
-export const templateId = "template_q8lg02n";
-export const publicKey = "FZhMGf9IcnQFVIvPZ";
+export const serviceId = "service_5twvrkg";
+export const templateId = "template_snyyzi4";
+export const publicKey = "IQSdcMcdSVTs5ae-R";
+
+// export const templateId = "template_i4czdlk";
+// export const publicKey = "q5kuONUVeDB8IDcCv";
+//  export const serviceId = "service_6vre85n";
