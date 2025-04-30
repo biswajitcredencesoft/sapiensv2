00d9015c7a091846dcf0ce3e654e51367b48dc61
Biswajit-Credencesoft <biswajit.sahoo@credencesoft.in>
2025-04-30 10:05:55 +0530

    service id changes

--- src/configs/emailjs.js
+++ src/configs/emailjs.js
@@ -1,7 +1,7 @@
-export const serviceId = "service_5twvrkg";
+export const serviceId = "service_utq3pbn";
 export const templateId = "template_snyyzi4";
 export const publicKey = "IQSdcMcdSVTs5ae-R";
 
 // export const templateId = "template_i4czdlk";
 // export const publicKey = "q5kuONUVeDB8IDcCv";
-//  export const serviceId = "service_6vre85n";
+// export const serviceId = "service_6vre85n";
