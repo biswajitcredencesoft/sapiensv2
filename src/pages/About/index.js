d6bf177c9a5676b8892ebf35014bd6fc81c493aa
Biswajit-Credencesoft <biswajit.sahoo@credencesoft.in>
2025-05-14 10:26:28 +0530

    font size changes in about us page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -7,14 +7,14 @@ const index = () => {
   return (
     <div className="about ">
       <div className="bg-[#edeeff] pb-0 md:pb-10 md:pt-10 pt-0">
-        <p className="text-center lg:text-2xl md:text-xl text-[18px] px-6 md:px-20 pt-10 text-[#000000]">
+        <p className="text-left lg:text-xl md:text-xl text-[18px] px-6 md:px-20 pt-10 text-[#000000]">
           Notwithstanding the hype around AI in recent years, its quintessential 
           ability to discern patterns and relationships across untenably large and
           complex datasets is powerful.
-          <br />
+       
           This clarity allows people to make confident decisions while alleviating
           cognitive limitations.
-          <br />
+          
           Cognisant of this and the research that has underpinned AI globally, we
           embarked on creating Stock seer.
         </p>
