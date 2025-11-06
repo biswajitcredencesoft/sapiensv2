c1bbb3ec23adef0a23d76fdf2b9e18b250d701f2
Biswajit-Credencesoft <170026678+biswajitcredencesoft@users.noreply.github.com>
2025-11-06 16:38:11 +0530

    sapiens  card changes

--- src/components/MeetTheTeam/index.js
+++ src/components/MeetTheTeam/index.js
@@ -1,16 +1,12 @@
-import React, { useState } from "react";
-// import Person1 from "../../assets/images/Joshua.webp";
-// import Person2 from "../../assets/images/Roland.webp";
-// import Person3 from "../../assets/images/damien.webp";
+import React from "react";
 import Person1 from "../../assets/images/Joshua1.svg";
 import Person2 from "../../assets/images/Roland1.svg";
-import Person3 from "../../assets/images/Damien1.svg";
-
 import Person4 from "../../assets/images/Kanika.svg";
 import Person5 from "../../assets/images/Vidhi.svg";
 import Person6 from "../../assets/images/Shruti.svg";
 
 import "./index.css";
+
 const MeetTheTeam = () => {
   const details = [
     {
@@ -32,14 +28,7 @@ const MeetTheTeam = () => {
       name: "Vidhi Jamar",
       designation: "VP Sales",
       description:
-        "Tasked with business development, Vidhi brings panache to her engagement with her customers and network.  Adept at identifying tangible workable business solutions that harness technology and in particular AI as an enabler she will help you reimagine your business potential and achieve measurable ROI. With deep dive insight into technologies both present and emerging she is an ally in your business journey. After work you may spot her chilling out at a classical music recital or at an award-winning winery.",
-    },
-    {
-      image: Person3,
-      name: "Damien Anselmi",
-      designation: "Consultant",
-      description:
-        "A chartered accountant by qualification, Damien brings more than 35 years of retail technology experience to Sapiens v2, specialising in Data Design, Analytics, and Data Visualization. Damien's focus at Sapiens v2 is Data Development, and he is a key contributor to product strategy and design.",
+        "Tasked with business development, Vidhi brings panache to her engagement with her customers and network. Adept at identifying tangible workable business solutions that harness technology and in particular AI as an enabler she will help you reimagine your business potential and achieve measurable ROI. With deep dive insight into technologies both present and emerging she is an ally in your business journey. After work you may spot her chilling out at a classical music recital or at an award-winning winery.",
     },
     {
       image: Person4,
@@ -56,6 +45,10 @@ const MeetTheTeam = () => {
         "Shruti’s background as a software engineer coupled with her business focused, people centric approach has allowed her to successfully lead and implement a myriad of projects across multiple verticals. As a people leader who communicates clearly and is an astute problem solver, the success of a project is all but guaranteed. When not working Shruti may be spotted trekking some picturesque trail on the planet.",
     },
   ];
+
+  const firstRow = details.slice(0, 3);
+  const secondRow = details.slice(3);
+
   return (
     <div className="pb-16">
       <p
@@ -64,49 +57,61 @@ const MeetTheTeam = () => {
       >
         Meet The Team
       </p>
-      <div className="grid md:grid-cols-3 md:gap-16 lg:mt-10 md:mt-10 gap-5 mx-3 md:mx-10 justify-content-center ">
-        {details.map((s, i) => {
-          return (
-            <div key={i} className="lg:w-full md:px-0 px-10  ">
-              <div className="">
-                <img src={s.image} className=" w-full mx-auto rounded-xl" />
-              </div>
-              <div className="text-center mt-3">
-                <p
-                  style={{ fontFamily: "AllroundGothic" }}
-                  className="lg:text-4xl md:text-4xl text-2xl text-[#000000] font-bold pb-2"
-                >
-                  {s.name}
-                </p>
-                <p
-                  style={{ fontFamily: "AllroundGothic" }}
-                  className="text-lg text-gray-700 font-semibold pb-2 "
-                >
-                  {s.designation}
-                </p>
-                {/* <p
-                  style={{
-                    fontFamily: "Roboto, sans-serif",
-                    letterSpacing: "0.25px",
-                    wordSpacing: "0px",
-                  }}
-                  className="text-justify text-[#000000] text-[17px] leading-[1.8]"
-                >
-                  {s.description}
-                </p> */}
 
-                {/* <p className="team-description">{s.description}</p> */}
-                <p
-                  className={`team-description ${
-                    s.name === "Damien Anselmi" ? "damien-description" : ""
-                  }`}
-                >
-                  {s.description}
-                </p>
-              </div>
+      <div className="grid md:grid-cols-3 md:gap-16 gap-5 mx-3 md:mx-10">
+        {firstRow.map((s, i) => (
+          <div key={i} className="lg:w-full md:px-0 px-10">
+            <div>
+              <img src={s.image} className="w-full mx-auto rounded-xl" />
             </div>
-          );
-        })}
+            <div className="text-center mt-3">
+              <p
+                style={{ fontFamily: "AllroundGothic" }}
+                className="lg:text-4xl md:text-4xl text-2xl text-[#000000] font-bold pb-2"
+              >
+                {s.name}
+              </p>
+              <p
+                style={{ fontFamily: "AllroundGothic" }}
+                className="text-lg text-gray-700 font-semibold pb-2"
+              >
+                {s.designation}
+              </p>
+              <p 
+               style={{ fontFamily: "roboto" }}
+              className="team-description">{s.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Second row (center aligned) */}
+      <div className="flex flex-wrap justify-center md:gap-16 gap-5 mx-3 md:mx-10 mt-10">
+        {secondRow.map((s, i) => (
+          <div
+            key={i}
+            className="lg:w-[30%] md:w-[40%] sm:w-[80%] px-10 md:px-0"
+          >
+            <div>
+              <img src={s.image} className="w-full mx-auto rounded-xl" />
+            </div>
+            <div className="text-center mt-3">
+              <p
+                style={{ fontFamily: "AllroundGothic" }}
+                className="lg:text-4xl md:text-4xl text-2xl text-[#000000] font-bold pb-2"
+              >
+                {s.name}
+              </p>
+              <p
+                style={{ fontFamily: "AllroundGothic" }}
+                className="text-lg text-gray-700 font-semibold pb-2"
+              >
+                {s.designation}
+              </p>
+              <p className="team-description">{s.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
